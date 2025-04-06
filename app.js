8902b5eba338bc13f7438a798192dc19751c8ed9
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-04-06 22:17:28 +0300

    chapter 13 - section 219 adding and using system user

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const mongoose = require('mongoose');
-
+const User = require('./models/user');
 const app = express();
 
 // express will support ejs as view engine when we wil use the function for dynamic templates
@@ -18,26 +18,42 @@ const e = require('express');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use((req, res, next) => {
-//     console.log(req.user);
-//     User.findById('67ec0b7063a4ca8a7476d153')
-//         .then(user => {
-//             console.log('logged User fetched');
-//             console.log(user);
+app.use((req, res, next) => {
+    console.log(req.user);
+    User.findById('67f2d0b1d9107d7534def5b8')
+        .then(user => {
+            console.log('logged User fetched');
+            console.log(user);
             
-//             req.user = new User(user.name, user.email, user.cart, user._id); 
-//             console.log(req.user);
+            req.user = user; 
+            console.log(req.user);
             
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// })
+            next();
+        })
+        .catch(err => console.log(err));
+})
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 mongoose.connect('***REMOVED***?appName=shop-db"')
 .then((result) => {
     console.log('Connected to Database');
+    User.findOne({name: 'system'})
+    .then(user => {
+        if (!user) {
+            return new User({
+                name: 'system', 
+                email: 'doNotReply@examle.com',
+                cart: {
+                    items: []
+                }
+            }).save();
+        }
+    })
+    return User.findOne({name: 'system'});
+})
+.then(user => {
+    console.log(user);
     app.listen(3000)
 })
 .catch(err => console.log(err));
