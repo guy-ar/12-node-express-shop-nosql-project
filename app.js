c6cf8c682a0c162f6fb352a353897b0f193fd093
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-04-06 21:11:37 +0300

    chapter 13 - section 213-215 creating product schema, saving product and fetching all products

--- app.js
+++ app.js
@@ -13,29 +13,29 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 // const errorController = require('./controllers/error');
 const e = require('express');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-    console.log(req.user);
-    User.findById('67ec0b7063a4ca8a7476d153')
-        .then(user => {
-            console.log('logged User fetched');
-            console.log(user);
+// app.use((req, res, next) => {
+//     console.log(req.user);
+//     User.findById('67ec0b7063a4ca8a7476d153')
+//         .then(user => {
+//             console.log('logged User fetched');
+//             console.log(user);
             
-            req.user = new User(user.name, user.email, user.cart, user._id); 
-            console.log(req.user);
+//             req.user = new User(user.name, user.email, user.cart, user._id); 
+//             console.log(req.user);
             
-            next();
-        })
-        .catch(err => console.log(err));
-})
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// })
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-mongoose.connect('mongodb+srv://guyNodeShop:tkGQBi2GtnHMYZxU@word-quest-db.jdoj9.mongodb.net/?retryWrites=true&w=majority&appName=shop-db')
+mongoose.connect('***REMOVED***?appName=shop-db"')
 .then((result) => {
     console.log('Connected to Database');
     app.listen(3000)
