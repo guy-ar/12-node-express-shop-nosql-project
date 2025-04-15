fed581f642045f5f9978fb679d5c43f98730ab62
guy arbily <guy.arbilly@gmail.com>
2025-04-15 13:53:24 +0300

    chapter 14 - section 241 setting loggedIn on the session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,20 @@
 exports.getLogin = (req, res, next) => {
-    console.log("Cookies: ");
-    console.log(req.get('Cookie'));
-    const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
-    console.log(isLoggedIn);
+    // console.log("Cookies: ");
+    // console.log(req.get('Cookie'));
+    // const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         docTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 
 }
 
 exports.postLogin = (req, res, next) => {
     // assume login is valid - later on we will validate the login
-    // set cookies for login
-    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+    // set on session flag for login
+    req.session.isLoggedIn = true;
     res.redirect('/');
 
 }
