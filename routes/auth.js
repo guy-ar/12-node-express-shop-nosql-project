6778aa6af82a21874034025bc07c7dc38be964a7
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-04-17 12:16:42 +0300

    chapter 15 - section 280 - adding token for reset password using crypto package and sending it by email as first step to reset user password

--- routes/auth.js
+++ routes/auth.js
@@ -15,5 +15,7 @@ router.post('/logout', authController.postLogout);
 
 router.get('/reset', authController.getReset);
 
+router.post('/reset', authController.postReset);
+
 module.exports = router;
  
